ac831b319d11fa6c5ae422c305fe4f7dbbb12560
conniepope <conniepope77@gmail.com>
2019-06-02 12:15:47 -0400

    buttons showing, still images & ratings showing

--- assets/javascript.js
+++ assets/javascript.js
@@ -25,47 +25,55 @@ $("button").on("click", function() {
     // then function
     .then(function(response){
         var results = response.data
+        console.log(queryURL);
+        console.log(response);
 
-        
+    // Looping through each result
+        for (var i = 0; i < topics.length; i++) {
+        // Creating a div tag with variable
+            var adventure = $("<div>");
+        // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + results[i].rating)
+        // Creating an image tag with variable
+            var photo = $("<img>")
+        // Setting the src attribute of the image to a property pulled off the result item
+            photo.attr("src", results[i].images.fixed_width_still.url)
+            $(this).attr("data-state", "still");
+            console.log(this)
+
+        // Appending the paragraph and image tag to the div
+            adventure.append(photo);
+            adventure.append(p);     
+        // Prependng the div to the HTML page in the ".pictures" div
+            $(".pictures").prepend(adventure)
+
+        // When the still image is clicked, it should animate. 
+
+            // photo.on("click", function() {
+            //     var state = $(this).attr("data-state");
+            //     if ("data-state" === "still"){
+            //         $(this).attr("src", results[i].images.fixed.width.url)
+            //         $(this).attr("data-state", "animate");
+
+            //         // user clicks the gif again, it should stop playing.
+            //     } else {
+            //         $(this).attr("src", results[i].images.fixed_width_still.url)
+            //         $(this).attr("data-state", "still");
+
+            //     }
+            // })
+        }
     })
+});
 
-// // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
-// // Under every gif, display its rating (PG, G, so on).
-// //    * This data is provided by the GIPHY API.
-// //    * Only once you get images displaying with button presses should you move on to the next step.
 
-// // Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
 
-//___________ example to reference_______________
-//       // After data comes back from the request
-//       .then(function(response) {
-//         console.log(queryURL);
 
-//         console.log(response);
-//         // storing the data from the AJAX request in the results variable
-//         var results = response.data;
 
-//         // Looping through each result item
-//         for (var i = 0; i < results.length; i++) {
+//    * Only once you get images displaying with button presses should you move on to the next step.
 
-//           // Creating and storing a div tag
-//           var animalDiv = $("<div>");
+// Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
 
-//           // Creating a paragraph tag with the result item's rating
-//           var p = $("<p>").text("Rating: " + results[i].rating);
 
-//           // Creating and storing an image tag
-//           var animalImage = $("<img>");
-//           // Setting the src attribute of the image to a property pulled off the result item
-//           animalImage.attr("src", results[i].images.fixed_height.url);
-
-//           // Appending the paragraph and image tag to the animalDiv
-//           animalDiv.append(p);
-//           animalDiv.append(animalImage);
-
-//           // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-//           $("#gifs-appear-here").prepend(animalDiv);
-//         }
-//       });
-//   });
+ 
